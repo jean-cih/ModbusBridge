0ce8b4f7af7d4e596d032f46651b8ef2686cf057
Evgeniy <evgenijpocekutov6@gmail.com>
2025-11-28 16:59:48 +0700

    Вынес настройку логера в отдельную функцию

--- Logger/logger.py
+++ Logger/logger.py
@@ -13,20 +13,23 @@ if os.path.exists(log_path):
 else:
     print(f"Папка '{log_path}' не существует, очистка не требуется.")
 
-os.makedirs(log_path)
+os.makedirs(log_path, exist_ok=True)
+
+def setup_logger(logger_name, level):
+    log = logging.getLogger(logger_name)
+
+    if not log.handlers:
+        file_handler = logging.FileHandler(f"Logger/log_status/{logger_name}.log", encoding='utf-8')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        log.addHandler(file_handler)
+        log.setLevel(level)
+    return log
 
 def logged(cls=None, *, name="", level=logging.INFO):
     def wrap(cls):
         logger_name = name or cls.__name__
-        cls.log = logging.getLogger(logger_name)
-
-        if not cls.log.handlers:
-            file_handler = logging.FileHandler(f"Logger/log_status/{logger_name}.log", encoding='utf-8')
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-            file_handler.setFormatter(formatter)
-            cls.log.addHandler(file_handler)
-            cls.log.setLevel(level)
-
+        cls.log = setup_logger(logger_name, level)
         return cls
 
     return wrap if cls is None else wrap(cls)
@@ -37,14 +40,7 @@ def log_function_call(name="", level=logging.DEBUG):
         @wraps(func)
         def wrapper(*args, **kwargs):
             logger_name = name or f"app.{func.__module__}.{func.__name__}"
-            log = logging.getLogger(logger_name)
-
-            if not log.handlers:
-                file_handler = logging.FileHandler(f"Logger/log_status/{logger_name}.log", encoding='utf-8')
-                formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-                file_handler.setFormatter(formatter)
-                log.addHandler(file_handler)
-                log.setLevel(level)
+            log = setup_logger(logger_name, level)
 
             log.log(level, f"Вызов функции {func.__name__} с args: {args}, kwargs: {kwargs}")
             try:
